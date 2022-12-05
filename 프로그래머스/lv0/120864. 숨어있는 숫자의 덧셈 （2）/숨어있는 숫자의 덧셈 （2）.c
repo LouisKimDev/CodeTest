2c48fe1ff7719f7b4be501645861bb964f23e6f1
DoyunKim <112796891+LouisKimDev@users.noreply.github.com>
2022-12-05 15:16:41 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.02 ms, Memory: 4.47 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(const char* my_str) {
+    int ans = 0, n = 0;
+    int t, i;
+    for(i = 0; my_str[i]; i++){
+        t = my_str[i];
+        if(t>='0'&& t<='9'){
+            n = n * 10 + t - '0';
+        }
+        else
+        {
+            ans += n; n = 0;
+        }
+            
+        printf("%d %d %c\n", n, ans, t);
+    }
+    ans += n;
+    return ans;
+
+}
