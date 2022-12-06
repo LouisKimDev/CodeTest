3a96ed82e048781863d8c759b80118afaad04a84
DoyunKim <112796891+LouisKimDev@users.noreply.github.com>
2022-12-06 17:46:10 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -1,15 +1,2 @@
 def solution(sides):
-    answer = 0
-    
-    i = max(sides) - min(sides)
-    j = max(sides) + min(sides)
-    
-    for x in range(1,max(sides)+1):
-        if(x>i and x<=max(sides)):
-            answer += 1
-            
-    for x in range(j):
-        if(x>max(sides)):
-            answer += 1
-            
-    return answer
+    return sum(sides) - max(sides) + min(sides) - 1
